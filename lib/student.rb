4fceb4a3a37c27dd13c8fbecebc5e15b469b7e6b
Jason Viger <jason.viger@gmail.com>
2018-08-24 09:39:19 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,7 +65,15 @@ class Student
   end
   
   def self.all_students_in_grade_9
-    
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+ 
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
   
 end
